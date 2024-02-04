bd89669a8f417d1ceed0f60f9d33fd9dcb9f290b
macOS <techasadbd@gmail.com>
2024-02-04 22:12:03 +0600

    cm

--- app/src/main/java/com/example/videos/AA.kt
+++ app/src/main/java/com/example/videos/AA.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.videos.databinding.AboutActivityBinding
 
-
 class AA : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
